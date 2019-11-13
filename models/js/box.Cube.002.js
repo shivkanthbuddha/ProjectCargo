0d55469c9bb2a06773ddc3637d65343905f418e2
Shiv Buddha <shivakanthb@gmail.com>
2019-11-13 09:29:23 +0530

    First commit

---
+++ models/js/box.Cube.002.js
@@ -0,0 +1,60 @@
+{
+
+	"metadata" :
+	{
+		"formatVersion" : 3.1,
+		"generatedBy"   : "Blender 2.7 Exporter",
+		"vertices"      : 8,
+		"faces"         : 6,
+		"normals"       : 8,
+		"colors"        : 0,
+		"uvs"           : [4],
+		"materials"     : 1,
+		"morphTargets"  : 0,
+		"bones"         : 0
+	},
+
+	"scale" : 5.000000,
+
+	"materials" : [	{
+		"DbgColor" : 15658734,
+		"DbgIndex" : 0,
+		"DbgName" : "Material.002",
+		"blending" : "NormalBlending",
+		"colorAmbient" : [0.6400000190734865, 0.6400000190734865, 0.6400000190734865],
+		"colorDiffuse" : [0.6400000190734865, 0.6400000190734865, 0.6400000190734865],
+		"colorEmissive" : [0.0, 0.0, 0.0],
+		"colorSpecular" : [0.5, 0.5, 0.5],
+		"depthTest" : true,
+		"depthWrite" : true,
+		"mapDiffuse" : "crate_1.jpg",
+		"mapDiffuseWrap" : ["repeat", "repeat"],
+		"shading" : "Lambert",
+		"specularCoef" : 50,
+		"transparency" : 1.0,
+		"transparent" : false,
+		"vertexColors" : false
+	}],
+
+	"vertices" : [-5,-5,-5,-5,5,-5,5,5,-5,5,-5,-5,-5,-5,5,-5,5,5,5,5,5,5,-5,5],
+
+	"morphTargets" : [],
+
+	"normals" : [-0.577349,0.577349,-0.577349,-0.577349,-0.577349,-0.577349,-0.577349,-0.577349,0.577349,-0.577349,0.577349,0.577349,0.577349,0.577349,0.577349,0.577349,0.577349,-0.577349,0.577349,-0.577349,0.577349,0.577349,-0.577349,-0.577349],
+
+	"colors" : [],
+
+	"uvs" : [[1,1,0,1,0,0,1,0]],
+
+	"faces" : [43,1,0,4,5,0,0,1,2,3,0,1,2,3,43,5,6,2,1,0,2,3,0,1,3,4,5,0,43,6,7,3,2,0,2,3,0,1,4,6,7,5,43,0,3,7,4,0,0,1,2,3,1,7,6,2,43,0,1,2,3,0,2,3,0,1,1,0,5,7,43,7,6,5,4,0,2,3,0,1,6,4,3,2],
+
+	"bones" : [],
+
+	"skinIndices" : [],
+
+	"skinWeights" : [],
+
+  "animations" : []
+
+
+}
